a9fb6e7e75950490fb77a75d34867db889a92d6d
alex28042 <alex28042@gmail.com>
2022-10-07 18:17:38 +0200

    desktop

--- App.jsx
+++ App.jsx
@@ -1,5 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Poppins_7000Bold } from '@expo-google-fonts/poppins';
+
+
 
 export default function App() {
   return (
